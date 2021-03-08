b5d6ac0ac5961acd5efde954972de44feaafcd7d
Andrea Undecimo <andreaundecimo.au@gmail.com>
2021-03-08 12:45:35 +0100

    add(FE): router

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
-import './App.css'
-import Login from './Components/Login/Login'
-import { StateContext, Store } from './globals/globalStore.reducer'
+import './App.css';
+import Login from './Components/Login/Login';
+import { Router } from '@reach/router';
+import { StateContext, Store } from './globals/globalStore.reducer';
+import Confirmation from './Components/Confirmation/Confirmation';
 
 function App() {
-  const [state, dispatch] = Store()
+  const [state, dispatch] = Store();
   return (
     <div className='App'>
       <StateContext.Provider value={{ state, dispatch }}>
-        <Login />
+        <Router style={{ height: '100%', overflow: 'hidden' }}>
+          <Login path='/' />
+          <Confirmation path='confirmation' />
+        </Router>
       </StateContext.Provider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
