d054f8081cc0defed28b6e451e35f568000ac378
Andrea Undecimo <andreaundecimo.au@gmail.com>
2021-03-08 12:45:07 +0100

    update email api

--- client/src/ApiServices/ApiClientRegister.js
+++ client/src/ApiServices/ApiClientRegister.js
@@ -6,6 +6,23 @@ export async function registerUser({ email }) {
   });
 }
 
-export async function completeAuthentication(token) {
+export async function getProfile(accessToken) {
+  return axios.get(`${process.env.REACT_APP_BASE_URL}/user/profile`, {
+    headers: { Authorization: accessToken },
+  });
+}
+
+export function completeAuthentication(token) {
   localStorage.setItem('token', token);
+}
+
+export async function sendEmailAgain({ newEmail, oldEmail }) {
+  return axios.put(`${process.env.REACT_APP_BASE_URL}/user/edit_email`, {
+    newEmail,
+    oldEmail,
+  });
+}
+
+export function handleLogout() {
+  localStorage.removeItem('token');
 }
