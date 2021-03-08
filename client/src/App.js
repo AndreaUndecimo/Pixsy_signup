d2ccc0267a1259652eebc44b74fbd634cf49f4df
Andrea Undecimo <andreaundecimo.au@gmail.com>
2021-03-08 07:36:40 +0100

    add(FE): global store

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import './App.css'
 import Login from './Components/Login/Login'
+import { StateContext, Store } from './globals/globalStore.reducer'
 
 function App() {
+  const [state, dispatch] = Store()
   return (
     <div className='App'>
-      <Login />
+      <StateContext.Provider value={{ state, dispatch }}>
+        <Login />
+      </StateContext.Provider>
     </div>
   )
 }
